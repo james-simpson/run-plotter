e6451c7b4f5531f86bb09d7263b033de06905c6d
James Simpson <jsimpson92@hotmail.co.uk>
2019-07-21 22:10:35 +0100

    More events tidy up

--- src/cljs/run_plotter/events.cljs
+++ src/cljs/run_plotter/events.cljs
@@ -1,41 +1,24 @@
 (ns run-plotter.events
-  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
     [re-frame.core :as rf]
     [run-plotter.db :as db]
-    [run-plotter.utils :as utils]
     [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]
     [day8.re-frame.undo :as undo]
-    [cljs-http.client :as http]
-    [cljs.core.async :refer [<!]]))
-
-(def ^:private api-base-url "http://localhost:3000")
+    [day8.re-frame.http-fx]
+    [ajax.core :as ajax]))
 
 (rf/reg-event-fx
   ::initialize-db
   (fn-traced [_ _]
              {:db db/default-db
-              :load-saved-routes []}))
+              :dispatch [:load-saved-routes]}))
 
-(defn- get-routes
-  []
-  (go (let [response (<! (http/get (str api-base-url "/routes")
-                                   {:as :json
-                                    :with-credentials? false}))
-            routes (:body response)]
-        (rf/dispatch [:set-saved-routes routes]))))
+(def ^:private api-base-url "http://localhost:3000")
 
 (rf/reg-event-db
   :set-active-panel
   (fn [db [_ active-panel]]
     (assoc db :active-panel active-panel)))
-
-(rf/reg-fx :load-saved-routes get-routes)
-
-(rf/reg-fx
-  :ajax
-  (fn [{:keys [db]} [_ routes]]
-    {:db (assoc db :saved-routes routes)}))
 
 (rf/reg-fx
   :show-toast
@@ -52,23 +35,6 @@
   (fn [db [_ lat lng]]
     (update-in db [:route :waypoints] #(concat % [[lat lng]]))))
 
-(defn- post-route!
-  [route]
-  (go (let [response (<! (http/post (str api-base-url "/routes")
-                                    {:json-params route
-                                     :with-credentials? false}))]
-        (if (:success response)
-          (rf/dispatch [:save-successful])
-          (utils/display-toast "Unable to save route" :failure)))))
-
-(defn- delete-route!
-  [id]
-  (go (let [response (<! (http/delete (str api-base-url "/routes/" id)
-                                      {:with-credentials? false}))]
-        (if (:success response)
-          (rf/dispatch [:delete-successful])
-          (utils/display-toast "Unable to delete route" :failure)))))
-
 (rf/reg-event-fx
   :set-saved-routes
   (fn [{:keys [db]} [_ routes]]
@@ -84,33 +50,6 @@
 (rf/reg-event-db
   :cancel-save
   #(assoc % :save-in-progress? false))
-
-(rf/reg-event-fx
-  :confirm-save
-  (fn [{:keys [db]} _]
-    (post-route! (:route db))
-    {:db (assoc db :save-in-progress? false)
-     :load-saved-routes []}))
-
-(rf/reg-event-fx
-  :save-successful
-  (fn [{:keys [db]} _]
-    {:db db
-     :load-saved-routes []
-     :show-toast ["Route saved" :success]}))
-
-(rf/reg-event-fx
-  :delete-route
-  (fn [{:keys [db]} [_ id]]
-    (delete-route! id)
-    {:db db}))
-
-(rf/reg-event-fx
-  :delete-successful
-  (fn [{:keys [db]} _]
-    {:db db
-     :load-saved-routes []
-     :show-toast ["Route deleted" :success]}))
 
 (rf/reg-event-db
   :clear-route
@@ -151,3 +90,75 @@
   :change-units
   (fn [db [_ units]]
     (assoc db :units units)))
+
+;;
+;; ajax
+;;
+
+;; get routes
+(rf/reg-event-fx
+  :load-saved-routes
+  (fn [_]
+    {:http-xhrio {:method :get
+                  :uri (str api-base-url "/routes")
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success [:get-routes-success]
+                  :on-failure [:get-routes-failure]}}))
+
+(rf/reg-event-db
+  :get-routes-success
+  (fn [db [_ routes]]
+    (assoc db :saved-routes routes)))
+
+(rf/reg-event-fx
+  :get-routes-failure
+  (fn [_]
+    {:show-toast ["Unable to fetch saved routes" :failure]}))
+
+;; post route
+(rf/reg-event-fx
+  :confirm-save
+  (fn [{:keys [db]} _]
+    {:db (assoc db :save-in-progress? false)
+     :http-xhrio {:method :post
+                  :uri (str api-base-url "/routes")
+                  :params (:route db)
+                  :format (ajax/json-request-format)
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success [:post-route-success]
+                  :on-failure [:post-route-failure]}}))
+
+(rf/reg-event-fx
+  :post-route-success
+  (fn [{:keys [db]} _]
+    {:db db
+     :dispatch [:load-saved-routes]
+     :show-toast ["Route saved" :success]}))
+
+(rf/reg-event-fx
+  :post-route-failure
+  (fn [_]
+    {:show-toast ["Unable to save route" :failure]}))
+
+;; delete route
+(rf/reg-event-fx
+  :delete-route
+  (fn [{:keys [db]} [_ id]]
+    {:http-xhrio {:method :delete
+                  :uri (str api-base-url "/routes/" id)
+                  :format (ajax/json-request-format)
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success [:delete-route-success]
+                  :on-failure [:delete-route-failure]}}))
+
+(rf/reg-event-fx
+  :delete-route-success
+  (fn [{:keys [db]} _]
+    {:db db
+     :dispatch [:load-saved-routes]
+     :show-toast ["Route deleted" :success]}))
+
+(rf/reg-event-fx
+  :delete-route-failure
+  (fn [_]
+    {:show-toast ["Unable to delete route" :failure]}))
