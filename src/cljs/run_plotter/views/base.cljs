df6f42e7286a5a42953d0ad60063b00eb6b50a1c
James Simpson <jsimpson92@hotmail.co.uk>
2019-09-01 12:29:44 +0100

    Fix navbar on mobile

--- src/cljs/run_plotter/views/base.cljs
+++ src/cljs/run_plotter/views/base.cljs
@@ -4,54 +4,40 @@
     [run-plotter.subs :as subs]
     [run-plotter.routes :as routes]
     [run-plotter.views.edit-route :refer [edit-route-panel]]
-    [run-plotter.views.saved-routes :refer [saved-routes-panel]]))
-
-(defn- radio-buttons
-  [{:keys [name selected-value on-change options]}]
-  [:div.field
-   (mapcat (fn [[value text]]
-             [^{:key value}
-             [:input.is-checkradio {:type "radio"
-                                    :name name
-                                    :id value
-                                    :checked (= value selected-value)
-                                    :on-change #(on-change value)}]
-              ^{:key (str value "-label")}
-              [:label {:for value} text]])
-           options)])
-
-(defn units-toggle
-  [units]
-  (radio-buttons {:name "units"
-                  :selected-value units
-                  :options [[:km "km"] [:miles "miles"]]
-                  :on-change (fn [value] (re-frame/dispatch [:change-units value]))}))
+    [run-plotter.views.saved-routes :refer [saved-routes-panel]]
+    [reagent.core :as reagent]))
 
 (defn- navbar-item
-  [route text active-panel]
+  [active-panel menu-expanded-atom route text]
   [:a.navbar-item {:href (routes/url-for route)
-                   :class (if (= active-panel route) "is-active" "")}
+                   :class (if (= active-panel route) "is-active" "")
+                   :on-click #(reset! menu-expanded-atom false)}
    text])
 
 (defn- navbar
   [active-panel units]
-  [:nav.navbar.is-info {:style {:height "5vh"}}
-   [:div.navbar-brand
-    [:a.navbar-item {:href (routes/url-for :edit-route)
-                     :style {:padding-left "20px"}}
-     [:img {:src "img/runner-icon.svg"}]]
-    [:div.navbar-burger.burger
-     [:span][:span][:span]]]
-   [:div.navbar-menu
-    [:div.navbar-start
-     [navbar-item :edit-route "Create a route" active-panel]
-     [navbar-item :saved-routes "Saved routes" active-panel]]
-    [:div.navbar-end
-     [:div.navbar-item
-      [units-toggle units]]
-     [:a.navbar-item {:href "https://github.com/jsimpson-github/run-plotter"
-                      :style {:margin-right "20px"}}
-      [:img {:src "img/github-logo.svg"}]]]]])
+  (let [menu-expanded? (reagent/atom false)]
+    (fn [active-panel units]
+      (let [menu-item (partial navbar-item active-panel menu-expanded?)]
+        [:nav.navbar.is-info
+         [:div.navbar-brand
+          [:a.navbar-item {:href (routes/url-for :edit-route)
+                           :style {:padding-left "20px"}}
+           [:img {:src "img/runner-icon.svg"}]]
+          [:div.navbar-burger.burger
+           {:class (if @menu-expanded? "is-active" "")
+            :on-click #(swap! menu-expanded? not)}
+           [:span] [:span] [:span]]]
+         [:div.navbar-menu {:class (if @menu-expanded? "is-active" "")}
+          [:div.navbar-start
+           [menu-item :edit-route "Create a route"]
+           [menu-item :saved-routes "Saved routes"]]
+          [:div.navbar-end
+           [:a.navbar-item {:href "https://github.com/jsimpson-github/run-plotter"
+                            :style {:margin-right "20px"}}
+            (if @menu-expanded?
+              "Github project"
+              [:img {:src "img/github-logo.svg"}])]]]]))))
 
 (defn base-view []
   (let [active-panel (re-frame/subscribe [::subs/active-panel])
